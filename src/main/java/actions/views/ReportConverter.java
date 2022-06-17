b62fa2f112394b400af9cc60287f9b8e7a92921b
Rena <Rena@192.168.10.102>
2022-06-17 14:01:00 +0900

    日報createアクションの作成

--- src/main/java/actions/views/ReportConverter.java
+++ src/main/java/actions/views/ReportConverter.java
@@ -5,13 +5,17 @@ import java.util.List;
 
 import models.Report;
 
-//日報データのDTOモデル⇔Viewモデルの変換を行うクラス
-
+/**
+ * 日報データのDTOモデル⇔Viewモデルの変換を行うクラス
+ *
+ */
 public class ReportConverter {
 
-    //ViewモデルのインスタンスからDTOモデルのインスタンスを作成する
-    //@param rv ReportViewのインスタンス
-    //@return Reportのインスタンス
+    /**
+     * ViewモデルのインスタンスからDTOモデルのインスタンスを作成する
+     * @param rv ReportViewのインスタンス
+     * @return Reportのインスタンス
+     */
     public static Report toModel(ReportView rv) {
         return new Report(
                 rv.getId(),
@@ -23,7 +27,11 @@ public class ReportConverter {
                 rv.getUpdatedAt());
     }
 
-    //DTOモデルのインスタンスからViewモデルのインスタンスを作成する
+    /**
+     * DTOモデルのインスタンスからViewモデルのインスタンスを作成する
+     * @param r Reportのインスタンス
+     * @return ReportViewのインスタンス
+     */
     public static ReportView toView(Report r) {
 
         if (r == null) {
@@ -40,9 +48,11 @@ public class ReportConverter {
                 r.getUpdatedAt());
     }
 
-    //DTOモデルのリストからViewモデルのリストを作成する
-    //@param list DTOモデルのリスト
-    //@return Viewモデルのリスト
+    /**
+     * DTOモデルのリストからViewモデルのリストを作成する
+     * @param list DTOモデルのリスト
+     * @return Viewモデルのリスト
+     */
     public static List<ReportView> toViewList(List<Report> list) {
         List<ReportView> evs = new ArrayList<>();
 
@@ -53,10 +63,11 @@ public class ReportConverter {
         return evs;
     }
 
-    //Viewモデルの全フィールドの内容をDTOモデルのフィールドにコピーする
-    //@param r DTOモデル(コピー先)
-    //@param rv Viewモデル(コピー元)
-
+    /**
+     * Viewモデルの全フィールドの内容をDTOモデルのフィールドにコピーする
+     * @param r DTOモデル(コピー先)
+     * @param rv Viewモデル(コピー元)
+     */
     public static void copyViewToModel(Report r, ReportView rv) {
         r.setId(rv.getId());
         r.setEmployee(EmployeeConverter.toModel(rv.getEmployee()));
@@ -67,4 +78,5 @@ public class ReportConverter {
         r.setUpdatedAt(rv.getUpdatedAt());
 
     }
+
 }
--- src/main/java/services/ReportService.java
+++ src/main/java/services/ReportService.java
@@ -11,15 +11,19 @@ import constants.JpaConst;
 import models.Report;
 import models.validators.ReportValidator;
 
-//日報テーブルの操作に関わる処理を行うクラス
-
+/**
+ * 日報テーブルの操作に関わる処理を行うクラス
+ */
 public class ReportService extends ServiceBase {
 
-    //指定した従業員が作成した日報データを、指定されたページ数の一覧画面に表示する分取得しReportViewのリストで返却する
-    //@param employee 従業員
-    //@param page ページ数
-    //@return 一覧画面に表示するデータのリスト
+    /**
+     * 指定した従業員が作成した日報データを、指定されたページ数の一覧画面に表示する分取得しReportViewのリストで返却する
+     * @param employee 従業員
+     * @param page ページ数
+     * @return 一覧画面に表示するデータのリスト
+     */
     public List<ReportView> getMinePerPage(EmployeeView employee, int page) {
+
         List<Report> reports = em.createNamedQuery(JpaConst.Q_REP_GET_ALL_MINE, Report.class)
                 .setParameter(JpaConst.JPQL_PARM_EMPLOYEE, EmployeeConverter.toModel(employee))
                 .setFirstResult(JpaConst.ROW_PER_PAGE * (page - 1))
@@ -28,9 +32,11 @@ public class ReportService extends ServiceBase {
         return ReportConverter.toViewList(reports);
     }
 
-    //指定した従業員が作成した日報データの件数を取得し、返却する
-    //@param employee
-    //@return 日報データの件数
+    /**
+     * 指定した従業員が作成した日報データの件数を取得し、返却する
+     * @param employee
+     * @return 日報データの件数
+     */
     public long countAllMine(EmployeeView employee) {
 
         long count = (long) em.createNamedQuery(JpaConst.Q_REP_COUNT_ALL_MINE, Long.class)
@@ -40,10 +46,13 @@ public class ReportService extends ServiceBase {
         return count;
     }
 
-    //指定されたページ数の一覧画面に表示する日報データを取得し、ReportViewのリストで返却する
-    //@param page ページ数
-    //@return 一覧画面に表示するデータのリスト
+    /**
+     * 指定されたページ数の一覧画面に表示する日報データを取得し、ReportViewのリストで返却する
+     * @param page ページ数
+     * @return 一覧画面に表示するデータのリスト
+     */
     public List<ReportView> getAllPerPage(int page) {
+
         List<Report> reports = em.createNamedQuery(JpaConst.Q_REP_GET_ALL, Report.class)
                 .setFirstResult(JpaConst.ROW_PER_PAGE * (page - 1))
                 .setMaxResults(JpaConst.ROW_PER_PAGE)
@@ -51,24 +60,30 @@ public class ReportService extends ServiceBase {
         return ReportConverter.toViewList(reports);
     }
 
-    //日報テーブルデータの件数を取得し、返却する
-    //returnデータの件数
+    /**
+     * 日報テーブルのデータの件数を取得し、返却する
+     * @return データの件数
+     */
     public long countAll() {
         long reports_count = (long) em.createNamedQuery(JpaConst.Q_REP_COUNT, Long.class)
                 .getSingleResult();
         return reports_count;
     }
 
-    //idを条件に取得したデータをReportViewのインスタンスで返却する
-    //@param id
-    //@return 取得データのインスタンス
+    /**
+     * idを条件に取得したデータをReportViewのインスタンスで返却する
+     * @param id
+     * @return 取得データのインスタンス
+     */
     public ReportView findOne(int id) {
         return ReportConverter.toView(findOneInternal(id));
     }
 
-    //画面から入力された日報の登録内容を元にデータを1件作成し、日報テーブルに登録する
-    //@param rv 日報の登録内容
-    //@return バリデーションで発生したエラーのリスト
+    /**
+     * 画面から入力された日報の登録内容を元にデータを1件作成し、日報テーブルに登録する
+     * @param rv 日報の登録内容
+     * @return バリデーションで発生したエラーのリスト
+     */
     public List<String> create(ReportView rv) {
         List<String> errors = ReportValidator.validate(rv);
         if (errors.size() == 0) {
@@ -82,9 +97,11 @@ public class ReportService extends ServiceBase {
         return errors;
     }
 
-    //画面から入力された日報の登録内容を元に、日報データを更新する
-    //@param rv 日報の更新内容
-    //@return バリデーションで発生したエラーのリスト
+    /**
+     * 画面から入力された日報の登録内容を元に、日報データを更新する
+     * @param rv 日報の更新内容
+     * @return バリデーションで発生したエラーのリスト
+     */
     public List<String> update(ReportView rv) {
 
         //バリデーションを行う
@@ -98,33 +115,43 @@ public class ReportService extends ServiceBase {
 
             updateInternal(rv);
         }
+
         //バリデーションで発生したエラーを返却（エラーがなければ0件の空リスト）
         return errors;
     }
 
-    //idを条件にデータを1件取得する
-    //@param id
-    //@return 取得データのインスタンス
+    /**
+     * idを条件にデータを1件取得する
+     * @param id
+     * @return 取得データのインスタンス
+     */
     private Report findOneInternal(int id) {
         return em.find(Report.class, id);
     }
 
-    //日報データを１件登録する
-    //@param rv 日報データ
+    /**
+     * 日報データを1件登録する
+     * @param rv 日報データ
+     */
     private void createInternal(ReportView rv) {
 
         em.getTransaction().begin();
         em.persist(ReportConverter.toModel(rv));
         em.getTransaction().commit();
+
     }
 
-    //日報データを更新する
-    //@param rv 日報データ
+    /**
+     * 日報データを更新する
+     * @param rv 日報データ
+     */
     private void updateInternal(ReportView rv) {
 
         em.getTransaction().begin();
         Report r = findOneInternal(rv.getId());
         ReportConverter.copyViewToModel(r, rv);
         em.getTransaction().commit();
+
     }
+
 }
